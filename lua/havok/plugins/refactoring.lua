cd1968fe5dd265e5f18f72e096122ca722a14f0a
DmytroKubatko <havoc3144@gmail.com>
2024-11-08 17:41:37 +0200

    add refactoring plugin

---
+++ lua/havok/plugins/refactoring.lua
@@ -0,0 +1,16 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  lazy = false,
+  config = function()
+    local refactoring = require("refactoring")
+    refactoring.setup()
+
+    vim.keymap.set({ "x", "n" }, "<leader>rv", function()
+      refactoring.debug.print_var()
+    end)
+  end,
+}
