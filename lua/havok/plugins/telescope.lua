5fb8bc4cebe65ba8459f1ef9adc5803ddab45fdc
DmytroKubatko <havoc3144@gmail.com>
2025-04-14 22:29:30 +0300

    add copy telescope selection mapping

--- lua/havok/plugins/telescope.lua
+++ lua/havok/plugins/telescope.lua
@@ -24,6 +24,17 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-y>"] = function()
+              local entry = require("telescope.actions.state").get_selected_entry()
+              local cb_opts = vim.opt.clipboard:get()
+              if vim.tbl_contains(cb_opts, "unnamed") then
+                vim.fn.setreg("*", entry.path)
+              end
+              if vim.tbl_contains(cb_opts, "unnamedplus") then
+                vim.fn.setreg("+", entry.path)
+              end
+              vim.fn.setreg("", entry.path)
+            end,
           },
         },
       },
@@ -39,6 +50,7 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "List open buffers" })
 
     keymap.set("n", "<leader>/", function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
