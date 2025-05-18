a1afd5d34591bdbed2aa086cca7903f5d6f928db
DmytroKubatko <havoc3144@gmail.com>
2025-05-18 20:44:19 +0300

    blink modified; mini adjusted

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -50,7 +50,7 @@ return { -- Autocompletion
       -- <c-k>: Toggle signature help
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      preset = 'enter',
+      preset = 'default',
 
       ['<C-k>'] = { 'select_prev' },
       ['<C-j>'] = { 'select_next' },
@@ -68,7 +68,8 @@ return { -- Autocompletion
     completion = {
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
+      accept = { auto_brackets = { enabled = false } },
     },
 
     sources = {
