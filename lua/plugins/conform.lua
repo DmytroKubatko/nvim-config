7a99abaeb185c833152a2f3a8e14e3bf53ebc96f
Dmytro Kubatko <d.kubatko@rozetkapay.com>
2025-06-09 16:31:02 +0300

    remove neotree; add vinegar (switch to use netrw); fix nvim-lint; add formating control to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,6 +1,6 @@
 return { -- Autoformat
   'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
+  event = { 'BufReadPre', 'BufNewFile' },
   cmd = { 'ConformInfo' },
   keys = {
     {
@@ -12,39 +12,62 @@ return { -- Autoformat
       desc = '[F]ormat buffer',
     },
   },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        return nil
+  config = function()
+    local conform = require 'conform'
+
+    conform.setup {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return nil
+        else
+          return {
+            timeout_ms = 500,
+            lsp_format = 'fallback',
+          }
+        end
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        css = { 'prettierd' },
+        html = { 'prettierd' },
+        json = { 'prettierd' },
+        yaml = { 'prettierd' },
+        markdown = { 'prettierd' },
+        graphql = { 'prettierd' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+    }
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
       else
-        return {
-          timeout_ms = 500,
-          lsp_format = 'fallback',
-        }
+        vim.g.disable_autoformat = true
       end
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      javascript = { 'prettierd' },
-      typescript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
-      css = { 'prettierd' },
-      html = { 'prettierd' },
-      json = { 'prettierd' },
-      yaml = { 'prettierd' },
-      markdown = { 'prettierd' },
-      graphql = { 'prettierd' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
-    },
-  },
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,37 +2,37 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    {
-      '<leader>ee',
-      function()
-        require('neo-tree.command').execute { toggle = true }
-      end,
-      desc = 'Explorer NeoTree (Root Dir)',
-    },
-    {
-      '<leader>ef',
-      function()
-        require('neo-tree.command').execute { toggle = true, reveal = true }
-      end,
-      desc = 'Explorer NeoTree (current file)',
-    },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+  -- 'nvim-neo-tree/neo-tree.nvim',
+  -- version = '*',
+  -- dependencies = {
+  --   'nvim-lua/plenary.nvim',
+  --   'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  --   'MunifTanjim/nui.nvim',
+  -- },
+  -- lazy = false,
+  -- keys = {
+  --   {
+  --     '<leader>ee',
+  --     function()
+  --       require('neo-tree.command').execute { toggle = true }
+  --     end,
+  --     desc = 'Explorer NeoTree (Root Dir)',
+  --   },
+  --   {
+  --     '<leader>ef',
+  --     function()
+  --       require('neo-tree.command').execute { toggle = true, reveal = true }
+  --     end,
+  --     desc = 'Explorer NeoTree (current file)',
+  --   },
+  -- },
+  -- opts = {
+  --   filesystem = {
+  --     window = {
+  --       mappings = {
+  --         ['\\'] = 'close_window',
+  --       },
+  --     },
+  --   },
+  -- },
 }
