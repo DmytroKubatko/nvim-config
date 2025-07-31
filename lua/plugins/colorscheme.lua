0d4fbb61f885e989f4ba7153d50c067c00f180cf
Dmytro Kubatko <d.kubatko@rozetkapay.com>
2025-07-31 18:43:21 +0300

    change onedark theme origin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,11 +9,13 @@
 -- }
 --
 return {
-  'one-dark/onedark.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {},
+  'navarasu/onedark.nvim',
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    require('onedark').setup()
+    require('onedark').setup {
+      style = 'warmer',
+    }
+    -- Enable theme
+    require('onedark').load()
   end,
 }
