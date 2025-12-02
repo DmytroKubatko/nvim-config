19594a99ded15a6d2932beee44ca9ff370103e40
Dmytro Kubatko <d.kubatko@rozetkapay.com>
2025-12-02 20:29:54 +0200

    more keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,3 +14,10 @@ set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Copy relative filepath to clipboard
+set('n', '<leader>cf', function()
+  local filepath = vim.fn.expand('%:.')
+  vim.fn.setreg('+', filepath)
+  print('Copied: ' .. filepath)
+end, { desc = '[C]opy relative [F]ilepath' })
